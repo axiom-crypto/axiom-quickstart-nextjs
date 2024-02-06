0dc002f414678941a020aef12657959adb342d43
Yu Jiang Tham <yoojbruin@gmail.com>
2024-02-06 16:18:07 -0500

    Remove unused comments

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,20 +1,4 @@
-import { classes } from "@/lib/utils"
 import Footer from "./Footer"
-
-
-{/* <div className={classes(
-  "flex flex-col justify-center items-center px-4",
-  "w-full sm:w-[40rem] lg:w-[64rem]"
-)}
->
-  <div className={classes(
-    "flex flex-col justify-center items-center w-full min-h-[40vh]",
-    "mt-10 px-8 py-4 border-[1px] border-darkgrey gap-8 shadow-md",
-  )}
-  >
-    {children}
-  </div>
-</div> */}
 
 export default function MainLayout({
   children,
