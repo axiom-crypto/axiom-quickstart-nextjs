cba6e21269379fd47a017382d7ae34419f9018ab
Yu Jiang Tham <yoojbruin@gmail.com>
2024-03-14 13:43:20 -0400

    Remove unused constants

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,4 @@
 export const Constants = Object.freeze({
   EXPLORER_BASE_URL: "https://explorer.axiom.xyz/v2/sepolia",
-  CALLBACK_CONTRACT: "0x50F2D5c9a4A35cb922a631019287881f56A00ED5",
   CHAIN_ID_SEPOLIA: 11155111,
 });
